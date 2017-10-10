58e74d2377fe61a5de2f366d8fff70c79064f17e
jacksoncharles <charles@jacksoncharles.co.uk>
2017-10-10 22:21:23 +0100

    new delete and get methods

---
+++ api/delete.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.delete = (event, context, callback) => {
+  
+  const params = {
+    TableName: process.env.POST_TABLE,
+    Key: {
+      id: event.pathParameters.id,
+    },
+  };
+
+  // delete the post from the database
+  dynamoDb.delete(params, (error) => {
+
+    // handle potential errors
+    if (error) {
+
+      console.error('=== error ===', error);
+
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t remove the post item.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({}),
+    };
+
+    callback(null, response);
+
+  });
+};
