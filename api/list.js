ff0e65b25ec9c5902bc02834b404751f23f2ef8c
jacksoncharles <charles@jacksoncharles.co.uk>
2017-10-12 21:48:58 +0100

    limit parameter

--- api/list.js
+++ api/list.js
@@ -3,30 +3,43 @@
 const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
-const params = {
+var params = {
   TableName: process.env.POST_TABLE,
+  Limit: 10
 };
 
 module.exports.list = (event, context, callback) => {
 
-  // fetch all posts from the database
-  dynamoDb.scan(params, (error, result) => {
-    // handle potential errors
-    if (error) {
-      console.error(error);
-      callback(null, {
-        statusCode: error.statusCode || 501,
-        headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t fetch the posts.',
-      });
-      return;
-    }
+     if( event.queryStringParameters !== null && typeof event.queryStringParameters === 'object' ) {
 
-    // create a response
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(result.Items),
-    };
-    callback(null, response);
-  });
+          if( event.queryStringParameters.hasOwnProperty('limit') ) {
+
+               params.Limit = event.queryStringParameters.limit;
+          }
+     }
+
+     // fetch all posts from the database
+     dynamoDb.scan(params, (error, result) => {
+
+          // handle potential errors
+          if (error) {
+
+               console.error(error);
+               callback(null, {
+                    statusCode: error.statusCode || 501,
+                    headers: { 'Content-Type': 'text/plain' },
+                    body: 'Couldn\'t fetch the posts.',
+               });
+
+               return;
+          }
+
+          // create a response
+          const response = {
+               statusCode: 200,
+               body: JSON.stringify(result.Items),
+          };
+
+          callback(null, response);
+     });
 };
